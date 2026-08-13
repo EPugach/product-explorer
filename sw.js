8c710a929d296b040ea3eb5692359961f267dc66
Eugene pugach <epugach@salesforce.com>
2026-08-13 00:20:55 -0400

    Bump DATA_CACHE pe-data-v8 -> v9 for the consumergoods KAM Agent fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const SHELL_VERSION = 20;
 const SHELL_CACHE = `pe-shell-v${SHELL_VERSION}`;
-const DATA_CACHE = "pe-data-v8";
+const DATA_CACHE = "pe-data-v9";
 
 const SHELL_URLS = [
   "./app/css/galaxy.css",
